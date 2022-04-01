acea5b21733f2a9e1d891ea491a296248818ffe4
zachl17 <zachluedtke17@gmail.com>
2022-04-01 03:41:25 -0600

    add turn taking

--- src/index.js
+++ src/index.js
@@ -2,15 +2,12 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 
-class Square extends React.Component {
-  render() {
-    return (
-      <button className="square"
-      onClick={() => this.props.onClick()}>
-        {this.props.value}
-      </button>
-    );
-  }
+function Square(props){
+  return (
+    <button className="square" onClick={props.onClick}>
+      {props.value}
+    </button>
+  )
 }
 
 class Board extends React.Component {
@@ -18,12 +15,16 @@ class Board extends React.Component {
     super(props);
     this.state = {
       squares: Array(9).fill(null),
+      xIsNext: true,
     };
   }
 handleClick(i) {
   const squares = this.state.squares.slice();
-  squares[i] = 'X';
-  this.setState({squares: squares}}
+  squares[i] = this.state.xIsNext ? 'X' : 'O';
+  this.setState({
+    squares: squares,
+    xIsNext: !this.state.xIsNext,
+  })
 }
 
   renderSquare(i) {
@@ -33,7 +34,8 @@ handleClick(i) {
   }
 
   render() {
-    const status = 'Next player: X';
+    const status = 'Next player: ' + (this.state.xIsNext ?
+    'X' : 'O');
     return (
       <div>
         <div className="status">{status}</div>
